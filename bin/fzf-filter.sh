f5afd261048cbc5f4a0c57b20c41fab0b1cabefa
Shota Nozaki <emonkak@gmail.com>
2022-11-06 12:36:34 +0900

    refactor: fzf-filter.sh

--- bin/fzf-filter.sh
+++ bin/fzf-filter.sh
@@ -1,15 +1,22 @@
-#!/bin/bash -eu
+#!/bin/env bash
+
+set -o errexit -o nounset
+
+if [[ "${TRACE-0}" == "1" ]]
+then
+    set -o xtrace
+fi
 
 limit=$1
-source="$("${@:2}")"
+command_output="$("${@:2}")"
 sequence=1
 
-while read line
+while read -r pattern
 do
-  echo "${source}" | fzf -f "$line" | head -n $limit | while read body
+  while read -r line
   do
-    echo $sequence $body
-  done
-  echo $sequence
-  sequence=$(( sequence + 1 ))
+    echo "${sequence}" "${line}"
+  done < <(fzf -f "${pattern}" <<< "${command_output}" | head -n ${limit})
+  echo ${sequence}
+  sequence=$(( ${sequence} + 1 ))
 done
